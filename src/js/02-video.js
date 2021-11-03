eaa146f9af7f4fbb65c21bc62d458b678a5c67a4
NikJulia <74920206+NikJulia@users.noreply.github.com>
2021-11-04 01:59:47 +0200

    hw-08

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,21 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const STORAGE_KEY = 'videoplayer-current-time';
+
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+
+player.on('timeupdate', throttle(onGetCurrentTime, 1000));
+player.on('play', onPlayFromSavedTime);
+
+function onGetCurrentTime() {
+    player.getCurrentTime().then(function(seconds) {
+        localStorage.setItem(STORAGE_KEY, seconds);
+    });
+}
+
+function onPlayFromSavedTime() {
+    const savedTime = localStorage.getItem(STORAGE_KEY);
+    player.setCurrentTime(savedTime);
+}
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,44 @@
+import throttle from 'lodash.throttle';
 
+const STORAGE_KEY = 'feedback-form-state';
+
+const form = document.querySelector('.js-feedback-form');
+const formData = {};
+
+// Отслеживаем на форме событие input
+// Делаем так, чтобы хранилище обновлялось не чаще чем раз в 500 миллисекунд
+form.addEventListener('input', throttle(onFormFieldsInput, 500));
+form.addEventListener('submit', onFormSubmit);
+populateFormFields();
+
+
+// Записываем в локальное хранилище объект с полями email и message, в которых сохраняем текущие значения полей формы 
+function onFormFieldsInput (event) {
+    formData[event.target.name] = event.target.value;
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+  }
+
+// При сабмите формы очищаем хранилище и поля формы
+// Выводи объект с полями email, message и текущими их значениями в консоль
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  const savedFieldsData = localStorage.getItem(STORAGE_KEY);
+  const parsedFieldsData = JSON.parse(savedFieldsData);
+  console.log(parsedFieldsData);
+
+  event.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
+}
+
+// При загрузке страницы проверяем состояние хранилища, и если там есть сохраненные данные, заполняем ими поля формы. В противном случае поля будут пустыми
+function populateFormFields() {
+  const savedFieldsData = localStorage.getItem(STORAGE_KEY);
+  const parsedFieldsData = JSON.parse(savedFieldsData);
+
+  if (savedFieldsData) {
+      const { email, message } = parsedFieldsData;
+      form.email.value = email;
+      form.message.value = message;
+  }
+}
